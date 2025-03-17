d20c6eee7f1e7bd6ff833dfdc8bb9b061715e697
Sandeep V Kollipara <46266688+sandeep-kollipara@users.noreply.github.com>
2025-03-16 22:52:33 -0400

    Re-uploading starter pack from <https://start.spring.io/>
    Maven, Java, SB 3.5.0 (Snapshot), Jar, 17.

---
+++ spring-boot-demo/src/test/java/com/redllow/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redllow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
